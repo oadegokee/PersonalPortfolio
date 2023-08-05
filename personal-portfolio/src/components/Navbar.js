5243167474f21052137683c0cc1b5f51bc9fdd28
Olaoluwa Adegoke <laoluadegoke@icloud.com>
2023-08-04 19:57:13 -0500

    incoporated light and dark theme

--- personal-portfolio/src/components/Navbar.js
+++ personal-portfolio/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from 'react-scroll';
+import DarkMode from "./DarkMode";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
@@ -25,7 +26,7 @@ const Navbar = () => {
   return (
     <nav className={color ? "nav nav-bg" : "nav"}>
       <h1><Link className={color ? "nav-h1 nav-h1-bg" : "nav-h1"} to="home" spy={true} smooth={true} onClick={handleNavItemClick}>Olaoluwa Adegoke</Link></h1>
-			
+			<DarkMode/>
       <ul className={click ? "nav-items active" : "nav-items"}>
         <li><Link className={color ? "nav-li nav-li-bg" : "nav-li"} to="home" spy={true} smooth={true} onClick={handleNavItemClick}>Home</Link></li>
         <li><Link className={color ? "nav-li nav-li-bg" : "nav-li"} to="about" spy={true} smooth={true} onClick={handleNavItemClick}>About</Link></li>
@@ -34,9 +35,9 @@ const Navbar = () => {
 
       <div className="collapse-show-menu" onClick={handleClick}>
         {click ? (
-          <FaTimes size={20} /> // show the x icon if clicked
+          <FaTimes size={20} className='x-icon'/> // show the x icon if clicked
         ) : (
-          <FaBars size={20} /> // else show the hamburger icon
+          <FaBars size={20} className='h-icon'/> // else show the hamburger icon
         )}
       </div>
     </nav>
